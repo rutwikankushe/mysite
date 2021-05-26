796329e0f8ce071f67e2cb4a27b83c1209928313
rutwikankushe <67552108+rutwikankushe@users.noreply.github.com>
2021-05-27 01:18:15 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '89_^2t7^9@ase3-+ffpxd1brbmsd!bbyv8(@lyar(4ovo@dtlv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','onlinecvtool.herokuapp.com']
 
